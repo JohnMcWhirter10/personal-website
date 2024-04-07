5f5aa874422e892f6aa091dfcd300077ba69d28a
JohnMcWhirter10 <118152018+JohnMcWhirter10@users.noreply.github.com>
2024-04-07 14:55:21 -0500

    initial commit

---
+++ dist/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+    return (React.createElement("div", { className: "App" },
+        React.createElement("header", { className: "App-header" },
+            React.createElement("p", null,
+                "Edit ",
+                React.createElement("code", null, "src/App.tsx"),
+                " and save to reload."))));
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+    ],
+  },
+};

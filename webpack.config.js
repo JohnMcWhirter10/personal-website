3719cd8f8269d06bff2f04cacd23f1b02e25d3fd
JohnMcWhirter10 <118152018+JohnMcWhirter10@users.noreply.github.com>
2024-04-07 15:12:03 -0500

    creating build

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,28 @@
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "ts-loader",
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    mode: isProduction ? "production" : "development",
+    entry: "./src/index.tsx",
+    output: {
+      path: path.resolve(__dirname, "docs"),
+      filename: "bundle.js",
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "ts-loader",
+          },
         },
-      },
-    ],
-  },
+      ],
+    },
+  };
 };
